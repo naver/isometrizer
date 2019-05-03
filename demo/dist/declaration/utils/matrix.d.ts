f8fb468d8e18ceab4996092da663fabf03d077cb
WoodNeck <saiyu915@naver.com>
2019-05-03 19:17:57 +0900

    :bookmark: release 0.0.1

---
+++ demo/dist/declaration/utils/matrix.d.ts
@@ -0,0 +1,20 @@
+export default class Matrix3x3 {
+    static readonly ZERO: Matrix3x3;
+    static readonly ONE: Matrix3x3;
+    private _values;
+    readonly values: number[];
+    constructor(values: number[]);
+    mul(other: Matrix3x3): this;
+    rotateX(degree: number): void;
+    rotateY(degree: number): void;
+    rotateZ(degree: number): void;
+    scale(props?: {
+        x: number;
+        y: number;
+        z: number;
+    }): void;
+    toString4x4(): string;
+    match(other: Matrix3x3): boolean;
+    private _multiply3x3;
+    private _degToRad;
+}
---
+++ dist/declaration/index.d.ts
@@ -0,0 +1,5 @@
+import Isometrizer from "./isometrizer";
+export * from "./types";
+export * from "./constants";
+export * from "./errors";
+export default Isometrizer;
